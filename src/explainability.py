1911fced54db6514e534a252fc3e0fbb1a5e506b
quantong3342836466 <3342836466@qq.com>
2025-12-11 20:15:49 +0800

    页面优化

--- src/explainability.py
+++ src/explainability.py
@@ -2,6 +2,8 @@ import pandas as pd
 import shap
 from catboost import CatBoostClassifier
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 解决中文显示为方块的问题
+plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 import numpy as np
 import os
 import sys
